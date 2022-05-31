0c3e764154bbb0536df86907a7f2fed23471406a
Hubert <1207820441@qq.com>
2022-05-31 11:49:24 +0800

    完成mybatis配置

---
+++ MailService/src/main/java/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.htc")
+@PropertySource("classpath:jdbc.properties")
+@Import({JdbcConfig.class, MybatisConfig.class})
+public class SpringConfig {
+}
