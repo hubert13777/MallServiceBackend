5e19fd74a1292f0e4d9d50bd540934e0323c877c
Hubert <1207820441@qq.com>
2022-05-31 23:40:47 +0800

    修改目录结构

--- MailService/src/main/java/com/htc/entity/Commodity.java
+++ MailService/src/main/java/com/htc/entity/Commodity.java
@@ -10,12 +10,106 @@ public class Commodity {
     private String tabs;            // varchar(60) DEFAULT NULL COMMENT '标签'
     private String description;     // varchar(40) DEFAULT NULL COMMENT '宣传描述'
     private float price;            // decimal(10,2) unsigned NOT NULL DEFAULT '99.99'
-    private String pictureUrl1;     // varchar(80) DEFAULT NULL,
-    private String pictureUrl2;     // varchar(80) DEFAULT NULL,
-    private String pictureUrl3;     // varchar(80) DEFAULT NULL,
-    private String specification;   // varchar(100) DEFAULT NULL COMMENT '细节',
-    private String generateInformation; // varchar(40) DEFAULT NULL,
-    private String iconUrl;         // varchar(80) DEFAULT NULL,
-    
-    
+    private String pictureUrl1;     // varchar(80) DEFAULT NULL
+    private String pictureUrl2;     // varchar(80) DEFAULT NULL
+    private String pictureUrl3;     // varchar(80) DEFAULT NULL
+    private String specification;   // varchar(100) DEFAULT NULL COMMENT '细节'
+    private String generateInformation; // varchar(40) DEFAULT NULL
+    private String iconUrl;         // varchar(80) DEFAULT NULL
+
+    public int getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(int goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTabs() {
+        return tabs;
+    }
+
+    public void setTabs(String tabs) {
+        this.tabs = tabs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getPictureUrl1() {
+        return pictureUrl1;
+    }
+
+    public void setPictureUrl1(String pictureUrl1) {
+        this.pictureUrl1 = pictureUrl1;
+    }
+
+    public String getPictureUrl2() {
+        return pictureUrl2;
+    }
+
+    public void setPictureUrl2(String pictureUrl2) {
+        this.pictureUrl2 = pictureUrl2;
+    }
+
+    public String getPictureUrl3() {
+        return pictureUrl3;
+    }
+
+    public void setPictureUrl3(String pictureUrl3) {
+        this.pictureUrl3 = pictureUrl3;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public String getGenerateInformation() {
+        return generateInformation;
+    }
+
+    public void setGenerateInformation(String generateInformation) {
+        this.generateInformation = generateInformation;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
 }
