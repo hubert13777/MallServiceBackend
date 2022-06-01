f2cc8424c09758a8f53348474f3df52bc2aa12b9
Hubert <1207820441@qq.com>
2022-06-01 12:49:22 +0800

    成功获取Commodity数据数据

--- MailService/src/main/java/com/htc/dao/CommodityDao.java
+++ MailService/src/main/java/com/htc/dao/CommodityDao.java
@@ -2,13 +2,15 @@ package com.htc.dao;
 
 import com.htc.entity.Commodity;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommodityDao {
-    @Select("select * from commodity;")
+    @Select("select * from Commodity;")
     public List<Commodity> selectAll();
 
-    @Select("select * from commodity where goodsId = #{id};")
+    @Select("select * from Commodity where goodsId = #{id};")
     public Commodity selectAllById(int id);
 }
--- MailService/src/main/java/com/htc/service/CommodityService.java
+++ MailService/src/main/java/com/htc/service/CommodityService.java
@@ -1,5 +1,22 @@
 package com.htc.service;
 
-public class CommodityService {
+import com.htc.dao.CommodityDao;
+import com.htc.entity.Commodity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class CommodityService {
+    @Autowired
+    private CommodityDao commodityDao;
+
+    public List<Commodity> getAllGoods(){
+        return commodityDao.selectAll();
+    }
+
+    public Commodity getGoodsById(int goodsId){
+        return commodityDao.selectAllById(goodsId);
+    }
 }
